107c1789d48a7b6c840dd56bd2a803662ed3d900
Kmasfkma <kyrlosadel@outlook.com>
2025-07-07 23:12:24 +0300

    Update site.ts

--- frontend/src/lib/site.ts
+++ frontend/src/lib/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: 'Kortix Suna',
-  url: 'https://suna.so/',
+  name: 'Kortix Nimind',
+  url: 'https://nimind.so/',
   description: 'Kortix AI',
   links: {
     twitter: 'https://x.com/kortixai',
